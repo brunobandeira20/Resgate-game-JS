3ea6beb522f34ac5b3bbd6afdc5ada4970e58938
brunobandeira20 <brunobandeira20@gmail.com>
2021-08-22 22:20:09 -0300

    fisrt

---
+++ js/js.js
@@ -0,0 +1,176 @@
+function start() {
+  $("#inicio").hide();
+
+  $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+  $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+  $("#fundoGame").append("<div id='inimigo2'></div>");
+  $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+  var jogo = {};
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 334);
+  var podeAtirar = true;
+  var fimDoJogo = false;
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+  }
+
+  jogo.pressionou = [];
+
+  // verifica se o usuario pressionou alguma tecla
+
+  $(document).keydown(function (e) {
+    jogo.pressionou[e.which] = true;
+  });
+
+
+  $(document).keyup(function (e) {
+    jogo.pressionou[e.which] = false;
+  });
+
+  //gameloop
+
+  jogo.timer = setInterval(loop, 30);
+
+  function loop() {
+
+    moveFundo();
+    moveinimigo1();
+    moveJogador();
+    moveInimigo2();
+    moveAmigo();
+    colisao();
+
+  }
+
+  function moveFundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position", esquerda - 2);
+  }
+  function moveJogador() {
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo - 10);
+
+      if (topo <= 0) {
+        $("#jogador").css("top", topo + 0);
+      }
+    }
+    if (jogo.pressionou[TECLA.S]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo + 10);
+
+      if (topo >= 434) {
+        $("#jogador").css("top", topo - 0)
+      }
+    }
+    if (jogo.pressionou[TECLA.D]) {
+      disparo();
+    }
+
+  }
+  function moveinimigo1() {
+
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left", posicaoX - velocidade);
+    $("#inimigo1").css("top", posicaoY);
+
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+
+    }
+  }
+  function moveInimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left", posicaoX - 3);
+
+    if (posicaoX <= 0) {
+      $("#inimigo2").css("left", 775);
+    }
+  }
+  function moveAmigo() {
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left", posicaoX + 1);
+
+    if (posicaoX > 906) {
+      $("#amigo").css("left", 0);
+    }
+  }
+  function disparo() {
+    if (podeAtirar == true) {
+
+      podeAtirar = false;
+
+      topo = parseInt($("#jogador").css("top"));
+      posicaoX = parseInt($("#jogador").css("left"));
+      tiroX = posicaoX + 190;
+      topoTiro = topo + 40;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top", topoTiro);
+      $("#disparo").css("left", tiroX);
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30);
+    }
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left", posicaoX + 15);
+      if (posicaoX > 900) {
+        window.clearInterval(tempoDisparo);
+        tempoDisparo = null;
+        $("#disparo").remove();
+        podeAtirar = true;
+      }
+    }
+  }
+  function colisao() {
+    var colisao1 = ($("#jogador").collision($("#inimigo1")));
+    var colisao2 = ($("#jogador").collision($("#inimigo2")));
+    var colisao3 = ($("#disparo").collision($("#inimigo1")));
+    var colisao4 = ($("#disparo").collision($("#inimigo2")));
+    var colisao5 = ($("#jogador").collision($("#amigo")));
+    var colisao6 = ($("#inimigo2").collision($("#amigo")));
+    if (colisao1.length > 0) {
+      inimigo1X = parseInt($("#inimigo1").css("left"));
+      inimigo1Y = parseInt($("#inimigo1").css("top"))
+      explosao1(inimigo1X, inimigo1Y);
+
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+
+
+    }
+    if (colisao2.length > 0) {
+
+      inimigo2X = parseInt($("#inimigo2").css("left"));
+      inimigo2Y = parseInt($("#inimigo2").css("top"));
+      explosao2(inimigo2X, inimigo2Y);
+
+      $("#inimigo2").remove();
+
+      reposicionaInimigo2();
+
+    }
+  }
+  function explosao1(inimigo1X, inimigo1Y) {
+    $("#fundoGame").append("<div id='explosao1'></div>");
+    $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+    var div = $("#explosao1");
+    div.css("top", inimigo1Y);
+    div.css("left", inimigo1X);
+    div.animate({ width: 200, opacity: 0 }, "slow");
+
+    var tempoExplosao = window.setInterval(removeExplosao, 1000);
+
+    function removeExplosao() {
+      div.remove();
+      window.clearInterval(tempoExplosao);
+      tempoExplosao = null;
+    }
+
+  }
+}
